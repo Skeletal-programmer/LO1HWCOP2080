ac4a548c171f0acaaa5bd12305348e14a07ad044
Grady Coffey <gcoffey0744@floridapoly.edu>
2026-01-19 02:42:10 +0000

    Created print_text.py to demonstrate the print function

---
+++ print_test.py
@@ -0,0 +1,5 @@
+my_name = "Grady Coffey"
+print(my_name)
+print("USe 4 spaces per level to maintain readability and consistency")
+print("Objects whose value can change are said to be mutable; objects whose value is unchangeable once they are created are called immutable.")
+print("I hope to expand my knowledge of python, learn how to apply python coding for game development, and become better as a coder and developer")
